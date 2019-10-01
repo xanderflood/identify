97cabcd7591c909cebb94a5109ae503ee168a4da
Xander Flood <xander.t.flood@gmail.com>
2019-09-30 21:44:28 -0400

    figaro and devise bootstrapping

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    # 302 (temporary redirect) to the catalog service
+    # this is temporary so that unauthenticated users don't
+    # get whisked away when they're trying to sign in.1
+    root to: 'home#index', name: "home"
+  end
+
+  unauthenticated do
+    devise_scope :user do
+      root to: "devise/sessions#new", name: "login"
+    end
+  end
 end
