f783a0d2a0a5748d9737ca1e275ab43d3b6cac96
Xander Flood <xander.t.flood@gmail.com>
2019-10-01 17:45:05 -0400

    re-tool the database to use UUIDs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   authenticated :user do
     # 302 (temporary redirect) to the catalog service
