ff965f59382593f91ef3594934a4baee362295ff
Xander Flood <xander.t.flood@gmail.com>
2019-10-07 20:37:17 -0400

    cofigure the mailserver

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,3 +1,7 @@
 Rails.application.configure do
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_options = {from: "no-reply@#{Rails.application.config.x.service_domain}"}
   config.action_mailer.default_url_options = { host: Rails.application.config.x.service_domain }
 end
