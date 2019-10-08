d453ab237da0439efc987ee314939d725824834e
Xander Flood <xander.t.flood@gmail.com>
2019-10-08 16:42:19 -0400

    fix SMTP config

--- config/application.rb
+++ config/application.rb
@@ -33,17 +33,24 @@ module Identify
     # load config from environment
     config.x.service_domain              = ENV["SERVICE_DOMAIN"]       || fail("Environment variable SERVICE_DOMAIN must be set")
     config.x.authorization_domain        = ENV["AUTHORIZATION_DOMAIN"] || fail("Environment variable AUTHORIZATION_DOMAIN must be set")
-    config.x.jwt_signing_secret          = ENV["JWT_SIGNING_SECRET"]
-    fail("Environment variable JWT_SIGNING_SECRET must be set") if defined?(::Rails::Server) && !config.x.jwt_signing_secret
+
+    if defined?(::Rails::Server)
+        config.x.smtp_address                = ENV["SMTP_ADDRESS"]         || fail("Environment variable SMTP_ADDRESS must be set")
+        config.x.service_domain              = ENV["SERVICE_DOMAIN"]       || fail("Environment variable SERVICE_DOMAIN must be set")
+        config.x.smtp_username               = ENV["SMTP_USERNAME"]        || fail("Environment variable SMTP_USERNAME must be set")
+        config.x.smtp_password               = ENV["SMTP_PASSWORD"]        || fail("Environment variable SMTP_PASSWORD must be set")
+        config.x.jwt_signing_secret          = ENV["JWT_SIGNING_SECRET"]   || fail("Environment variable JWT_SIGNING_SECRET must be set")
+        config.x.smtp_port                   = ENV["SMTP_PORT"].to_i > 0 ? ENV["SMTP_PORT"].to_i : fail("Environment variable SMTP_PORT must be set")
+    end
 
     # NOTE: blank default_referrer_url causes it to use '/manage',
     # which has account management links
     config.x.default_referrer_url              = ENV["DEFAULT_REFERRER_URL"]
     config.x.referrer_cookie_name              = ENV["REFERRER_COOKIE_NAME"] || "_identify_referrer_url"
     config.x.jwt_cookie_name                   = ENV["JWT_COOKIE_NAME"]      || "_identify_jwt_string"
-    config.x.referrer_cookie_ttl_seconds       = ENV["REFERRER_COOKIE_EXPIRATION_SECONDS"] ? ENV["REFERRER_COOKIE_EXPIRATION_SECONDS"].to_i : 1800  # 30 minutes
-    config.x.jwt_ttl_seconds                   = ENV["JWT_COOKIE_EXPIRATION_SECONDS"]      ? ENV["JWT_COOKIE_EXPIRATION_SECONDS"].to_i      : 21600 # 6 hours
-    config.x.identify_sessions_timeout_seconds = ENV["IDENTIFY_SESSIONS_TIMEOUT_SECONDS"]  ? ENV["IDENTIFY_SESSIONS_TIMEOUT_SECONDS"]       : 86400 # 1 day
+    config.x.referrer_cookie_ttl_seconds       = ENV["REFERRER_COOKIE_EXPIRATION_SECONDS"].to_i > 0 ? ENV["REFERRER_COOKIE_EXPIRATION_SECONDS"].to_i : 1800  # 30 minutes
+    config.x.jwt_ttl_seconds                   = ENV["JWT_COOKIE_EXPIRATION_SECONDS"].to_i > 0      ? ENV["JWT_COOKIE_EXPIRATION_SECONDS"].to_i      : 21600 # 6 hours
+    config.x.identify_sessions_timeout_seconds = ENV["IDENTIFY_SESSIONS_TIMEOUT_SECONDS"].to_i > 0  ? ENV["IDENTIFY_SESSIONS_TIMEOUT_SECONDS"].to_i  : 86400 # 1 day
 
     config.x.notifications_source_email = "no-reply@#{config.x.service_domain}"
   end
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,5 +1,14 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              config.x.smtp_address,
+    port:                 config.x.smtp_port,
+    domain:               config.x.service_domain,
+    user_name:            config.x.smtp_username,
+    password:             config.x.smtp_password,
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
