a68c17f3059c72ba09dfa045f4fd64b28a5fef17
Xander Flood <xander.t.flood@gmail.com>
2019-10-07 20:53:49 -0400

    add the delivery_method and configure devise

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,8 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_options = {from: "no-reply@#{Rails.application.config.x.service_domain}"}
-  config.action_mailer.default_url_options = { host: Rails.application.config.x.service_domain }
+  config.action_mailer.default_options = { from: config.x.notifications_source_email }
+  config.action_mailer.default_url_options = { host: config.x.service_domain }
 end
